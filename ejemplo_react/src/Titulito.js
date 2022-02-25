d78e7f632589b7bfef4ae615c65d8dad9c4f465a
enti <enti@enti.enti>
2022-02-25 18:19:55 +0100

    Ejercicio

---
+++ ejemplo_react/src/Titulito.js
@@ -0,0 +1,17 @@
+
+
+function Titulito  () 
+{
+
+ return(
+ 	<div id="titulo">
+	<h1>Ola k ase</h1>
+	<p><strong><em>El mejor titulo de todos</em></strong></p>
+	
+	</div>
+ );
+}
+
+
+
+export default Titulito;
